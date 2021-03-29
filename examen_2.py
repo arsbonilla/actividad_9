d64e8b0314eddd6e21cbe8acefd064e404b89009
arsbonilla <76706958+arsbonilla@users.noreply.github.com>
2021-03-29 00:24:21 -0600

    Add files via upload

--- examen_2.py
+++ examen_2.py
@@ -5,27 +5,13 @@ Created on Sun Mar 28 17:32:17 2021
 @author: Luna_21
 """
 ###abrir imagenes
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import os
-import snappy
-from snappy import Product
-from snappy import ProductIO
-from snappy import ProductUtils
-from snappy import WKTReader
-from snappy import HashMap
-from snappy import GPF
-# Para leer shapefiles
-import shapefile
-import pygeoif
+
 import tkinter
 from tkinter import filedialog
 
 ventana= tkinter.Tk()
 ventana.geometry("600x600")
-
+#implementacion de las funciones que implementaran la ruta actual de los archivos
 def abririmagen():
     filepathimage= filedialog.askopenfilename(filetypes=[("Zip Files", "*.zip")])
     print(filepathimage)
@@ -33,12 +19,16 @@ def abrircapa():
     filepathcapa=filedialog.askopenfilename(filetypes=[("Shape File", "*.shp")])
     print(filepathcapa)
     
-boton1= tkinter.Button(ventana, text="Abrir Imagen", command=abririmagen)
-boton2= tkinter.Button(ventana, text="Abrir Capa", command=abrircapa)
+lbl1=   tkinter.Label(ventana, text="¿En donde se encuentra la imagen?").pack()
+boton1= tkinter.Button(ventana, text="Abrir Imagen", command=abririmagen).pack()
+Text1= tkinter.Text(ventana,width=50,height=1).pack()
+boton2= tkinter.Button(ventana, text="Abrir Capa", command=abrircapa).pack()
+Text2= tkinter.Text(ventana,width=50,height=1).pack()
 
-boton1.grid(row=0, column=0)
-boton2.grid(row=3, column=0)
+boton3=tkinter.Button(ventana, text="Procesar Imagen", command=preproceso).pack()
+
+
 ventana.mainloop()
 
-######codigo para procesar la imagen
-
+def preproceso():
+    
